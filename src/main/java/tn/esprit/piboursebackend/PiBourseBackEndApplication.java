622a74f363a9a7bad284b4b559ba0e7e7ccdbc1e
FerielBenMamia <160123556+Ferielbm@users.noreply.github.com>
2025-10-05 20:48:30 +0100

    version 0

---
+++ src/main/java/tn/esprit/piboursebackend/PiBourseBackEndApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.piboursebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiBourseBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiBourseBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/piboursebackend/PiBourseBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.piboursebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiBourseBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
